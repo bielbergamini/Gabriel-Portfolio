aa1f21bc923e8cdf028000e542bad055d414e501
Gabriel Bergamini <gabrielbergamini05@gmail.com>
2025-04-22 22:36:21 -0300

    Criando a página inicial

--- projetos/models.py
+++ projetos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Projeto(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to='projetos/', blank=True, null=True)
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.titulo
+
--- projetos/views.py
+++ projetos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Projeto
 
-# Create your views here.
+def listar_projetos(request):
+    projetos = Projeto.objects.all()
+    return render(request, 'projetos/listar_projetos.html', {'projetos': projetos})
+
+
